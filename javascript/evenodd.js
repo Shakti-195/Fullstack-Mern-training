34f44cdeae94ae6aff583b8edfb6d794da4f46eb
Shakti-195 <thakurshaktisingh195@gmail.com>
2026-07-02 22:14:24 +0530

    praticed new tags in css

---
+++ javascript/evenodd.js
@@ -0,0 +1,10 @@
+const prompt=require("prompt-sync")
+let num = Number(prompt("Enter a number"));
+// let num = 1;
+
+
+if (num % 2 === 0){
+    console.log("Even")
+}else{
+    console.log("odd");
+}
---
+++ javascript/script.js
@@ -0,0 +1,68 @@
+ function showMessage(){
+            
+
+            // variable in js
+             //variable declaration
+            let a = 23;
+            let b = "Name"
+            let c = true;
+            let d;
+            let e = null;
+            let f = [1,2,3];
+            let g= {
+                name:"Shakti",
+            }
+            let h = function name(){
+            
+            }
+
+                console.log(typeof(a));
+                console.log(typeof(b));
+                console.log(typeof(c));
+                console.log(typeof(d));
+
+            
+}
+
+// defining a function with parameter
+function operation(){
+
+}
+operation();// function call with arguments
+
+showMessage();
+
+let age = 21;
+let name = "Shakti";
+let isStudent = true;
+
+console.log(age);
+console.log(name);
+console.log(isStudent);
+
+// Arrays
+
+let fruits = ["Apple","Banana","Mango"];
+console.log(fruits);
+console.log(fruits[0]);
+
+// objects
+let student = {
+    name:"Shakti",
+    age:21,
+    course:"B-tech"
+}
+
+console.log(student);
+
+// Functions
+function add(a,b){
+    return a  + b;
+}
+
+console.log(add(10,20));
+
+// anonymous function
+let a = function(){
+
+}
