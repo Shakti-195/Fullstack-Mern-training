46c8cde680b7df0615b39bdf18418d45277fcf94
Shakti-195 <thakurshaktisingh195@gmail.com>
2026-07-09 06:06:46 +0530

    promises

--- javascript/string.js
+++ javascript/string.js
@@ -2,6 +2,8 @@
 // let str1 ="Shakti";
 // let str2 ="Singh";
 
+
+
 // console.log("my name is " + str1 + " " + str2)
 
 // template literals 
@@ -125,8 +127,7 @@
 //     age:21,
 //     gender:"Male",
 //     },
-    
-    
+
 //     address:{
 //         city: "Lucknow",
 //         state:"u.p",
@@ -140,17 +141,149 @@
 // console.log(Object.entries(obj.Persondetails));
 
 
-let obj1 = {
-       name:"Shakti Singh",
-       age:21,
-    
-}
+// let obj1 = {
+//        name:"Shakti Singh",
+//        age:21,
 
-let obj2 = {
-       ...obj1,
-       country:"India",
-       State:"Uttar Pradesh"
-    
-}
-console.log(obj2.age);
-console.log(obj2.name);
+// }
+
+// let obj2 = {
+//        ...obj1,
+//        country:"India",
+//        State:"Uttar Pradesh"
+
+// }
+// console.log(obj2.age);
+// console.log(obj2.name);
+
+// callback 
+
+// function greet(name,callback){
+//        console.log(`Hi i am ${name}`);
+//        callback();
+// }
+// function happen(){
+//        console.log("Task Completed");
+// }
+
+// greet("Shakti",happen);
+
+// function welcome(name,callback){
+//        console.log(`welcome ${name}`);
+//        callback();
+
+// }
+// function done(){
+//        console.log("Done!")
+       
+// }
+// welcome("Shakti",done);
+
+// function checkresult(marks,callback){
+//        let result = marks>40?'Student Passed':'Student Failed';
+//        let remarks = marks >= 40 ? "Excellent!" : "Better Luck Next Time";
+//        callback(result,marks,remarks);
+// }
+
+// function happen(result,marks,remarks){
+       
+//        console.log("Result:", result);
+//        console.log("Marks:", marks);
+//        console.log("Remarks:", remarks);
+// }
+
+// checkresult(25,happen);
+
+// function greeet (){
+//        console.log("I am a Full-Stack Software Developer...");
+
+// }
+// setTimeout(greeet,1000);
+
+// using set timeout print logging, is login successfull -3s
+// logout -5s
+
+// function login(){
+//        console.clear();
+//        console.log("login successfull..")
+       
+// }
+// function logout(){
+//        console.clear();
+//        console.log("oops logout..")
+// }
+
+// setTimeout(login,3000)
+// setTimeout(logout,5000)
+
+// setTimeout(() =>{
+//        console.log("Step1:Task1 uncomplete...");
+//        setTimeout(() =>{
+//               console.log("Step2:Task2 uncomplete...");
+//               setTimeout(() =>{
+//                      console.log("Step3:Task3 uncomplete...");
+//                      setTimeout(() => {
+//                             console.log("Step4:Task4 completed...");
+//                      },4000)
+//               },3000)
+//        },2000)
+// },1000)
+
+// function step1(){
+//        console.log("step1:Task1 uncomplete...");
+//        setTimeout(step2,2000);
+// }
+// function step2(){
+//        console.log("step2:Task2 uncomplete...");
+//        setTimeout(step3,3000);
+// }
+// function step3(){
+//        console.log("step3:Task3 uncomplete...");
+//        setTimeout(step4,4000);
+// }
+// function step4(){
+//        console.log("step4:Task4 uncomplete...");
+// }
+// setTimeout(step1,1000);
+
+// student registration 
+// 1.registerStudent
+// 2.verifyOTP
+// 3.generateStudentid
+
+// 
+
+// promises
+// let promise = new Promise((resolve,reject) => {
+//        if(true){
+//               resolve("Promise resolved");
+
+//        }else{
+//               reject("Promises rejected ");
+
+//        }
+// })
+
+// promise.then((result) => {
+//        console.log("Result");
+
+// }).catch((error)=>{
+//        console.log("Error");
+
+// })\
+// student result
+
+let  marks = 75;
+let promise = new Promise((resolve,reject)=>{
+       if(marks>40){
+              resolve("Student Passed");
+       }else{
+              reject("Student failed");
+       }
+})
+
+promise.then((result)=>{
+       console.log(result);
+}).catch((error)=>{
+       console.log(error);
+});
